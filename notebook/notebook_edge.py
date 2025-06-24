68332125a6fa0f50e2f8bfe78bc8f7c7e1eb3dfb
Revital Sur <eres@il.ibm.com>
2025-06-24 11:52:14 +0200

    Update notebook_edge.py.
    Signed-off-by: Revital Sur <eres@il.ibm.com>

--- notebook/notebook_edge.py
+++ notebook/notebook_edge.py
@@ -3,46 +3,55 @@ import json
 import requests
 import json
 import os
-import sys
 import numpy as np
+import boto3
+import io
 
-def infer():
-    #array = torch.load('./reduced_tronchetto_array.pt')
-    path = sys.argv[1] 
-    print(path)
-    array = torch.load(path)
+bucket_name = os.getenv("BUCKET_NAME")
+key = os.getenv("FILENAME")
 
-    # Create request message to be sent to the predictor
-    message_data = {}
-    inputs = {}
-    message_data["inputs"] = []
-    inputs["name"]="input1"
-    inputs["shape"] = array.shape
-    inputs["datatype"]="FP32" # as the given per model expects float32
-    inputs["data"]=array.tolist()
-    message_data["inputs"].append(inputs)
-    # Call predictor
+s3 = boto3.client(
+    's3',
+    endpoint_url=os.getenv("ENDPOINT"),
+    aws_access_key_id=os.getenv("ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("SECRET_KEY"),
+)
+# Get the object from S3
+response = s3.get_object(Bucket=bucket_name, Key=key)
 
-    service_hostname=os.environ["SERVICE_HOSTNAME"]
-    model_name=os.environ["MODEL_NAME"]
-    ingress_ip="localhost"
-    ingress_port=os.environ["INGRESS_PORT"]
-    predictor_url = f"http://{ingress_ip}:{ingress_port}/v2/models/{model_name}/infer"
-    request_headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Host": service_hostname,
-    }
-    print(json.dumps(message_data))
-    response = requests.post(predictor_url, headers=request_headers, data=json.dumps(message_data))
-    print(response)
-    response_message = json.loads(response.text)
-    output1 = np.array(response_message["outputs"][0]['data'], dtype=np.float32)
-    print(output1)
+# Read binary stream and load with torch
+buffer = io.BytesIO(response['Body'].read())
+array = torch.load(buffer, map_location='cpu')  # or 'cuda' if needed
+#array = torch.load('./reduced_tronchetto_array.pt')
 
+# Create request message to be sent to the predictor
+message_data = {}
+inputs = {}
+message_data["inputs"] = []
+inputs["name"]="input1"
+inputs["shape"] = array.shape
+inputs["datatype"]="FP32" # as the given per model expects float32
+inputs["data"]=array.tolist()
+message_data["inputs"].append(inputs)
+print(message_data)
+# Call predictor
 
-# Run it locally
-if __name__ == "__main__":
-    infer()
+service_hostname=os.environ["SERVICE_HOSTNAME"]
+model_name=os.environ["MODEL_NAME"]
+ingress_ip="localhost"
+ingress_port=os.environ["INGRESS_PORT"]
+predictor_url = f"http://{ingress_ip}:{ingress_port}/v2/models/{model_name}/infer"
+print(predictor_url)
+request_headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Host": service_hostname,
+}
+print(json.dumps(message_data))
+response = requests.post(predictor_url, headers=request_headers, data=json.dumps(message_data))
+print(response)
+response_message = json.loads(response.text)
+output1 = np.array(response_message["outputs"][0]['data'], dtype=np.float32)
 
-
+# postprocess
+print(output1)
